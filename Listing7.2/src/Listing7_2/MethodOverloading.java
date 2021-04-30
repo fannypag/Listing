acb00c0434da562b4742c1bec70324f96225e3fc
fannypag <81959515+fannypag@users.noreply.github.com>
2021-04-30 22:44:07 +0700

    Add files via upload

---
+++ Listing7.2/src/Listing7_2/MethodOverloading.java
@@ -0,0 +1,14 @@
+package Listing7_2;
+
+class MethodOverloading {
+
+	public static void main (String args []) 
+	{
+		Overload Obj = new Overload(); 
+		double result; 
+		Obj .demo(10); 
+		Obj .demo (10, 20); 
+		result = Obj .demo(5.5);
+		System.out.println("0/P : " + result); 
+	}
+}
